11707212ae328a53a392bacb5832f021bf03f636
HeyShafty <nathan.lecorchet@epitech.eu>
2020-09-14 15:04:38 +0200

    Gitignoring & ctrl magie

--- main.cpp
+++ main.cpp
@@ -2,20 +2,20 @@
 // EPITECH PROJECT, 2018
 // babel
 // File description:
-// 
+// babel
 //
 
-#include <stdio.h>
-#include <opus/opus.h>
-#include <portaudio.h>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QPushButton>
+#include <opus/opus.h>
+#include <portaudio.h>
+#include <stdio.h>
 
 int main(int argc, char **argv)
 {
-	QApplication app (argc, argv);
-	QPushButton button ("Hello world !");
+    QApplication app(argc, argv);
+    QPushButton button("Hello world !");
 
-	button.show();
-	return app.exec();
+    button.show();
+    return app.exec();
 }
