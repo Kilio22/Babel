79b15719f43aff2eb7c82a7ba7da4dd671a8562e
Kylian Balan <kylian.balan@epitech.eu>
2020-09-18 10:38:19 +0200

    adding includes to test on windows

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -6,6 +6,23 @@
 */
 
 #include <iostream>
+#include <QtGui/QWidget>
+#include <QtGui/QMainWindow>
+#include <QtGui/QPushButton>
+#include <QtGui/QStatusBar>
+#include <QtGui/QMenuBar>
+#include <QtGui/QLabel>
+#include <QtCore/QFile>
+#include <QtGui/QFileDialog>
+#include <QtGui/QTextEdit>
+#include <cstdlib>
+#include <QtCore/QTextStream>
+#include <string>
+#include <QtGui/QMessageBox>
+#include <QtGui/QFontDialog>
+#include <QtGui/QFont>
+#include <QtGui/QColorDialog>
+#include <QtGui/QApplication>
 
 int main(void)
 {
