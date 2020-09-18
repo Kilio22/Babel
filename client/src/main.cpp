315b49204f55afa104e78049898dcbbf8255a004
HeyShafty <nathan.lecorchet@epitech.eu>
2020-09-18 13:40:06 +0200

    Du cmake.

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -5,11 +5,31 @@
 ** main
 */
 
-#include <iostream>
+#include <QApplication>
+#include <QColorDialog>
+#include <QFile>
+#include <QFileDialog>
+#include <QFont>
+#include <QFontDialog>
+#include <QLabel>
+#include <QMainWindow>
+#include <QMenuBar>
+#include <QMessageBox>
+#include <QPushButton>
+#include <QStatusBar>
+#include <QTextEdit>
+#include <QTextStream>
 #include <QWidget>
+#include <opus.h>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <map>
 
 int main(void)
 {
+    std::map<int, int> mymap = {{1, 1}, {2, 2}};
+
     std::cout << "hello, world!" << std::endl;
-    return 0;
+    return mymap.contains(1);
 }
