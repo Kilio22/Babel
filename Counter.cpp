93f751ec9f68a5f6b5a1aa13b16b8c45d224f385
Kylian Balan <kylian.balan@epitech.eu>
2020-09-18 10:07:07 +0200

    Adding projet tree

--- Counter.cpp
+++
@@ -1,9 +0,0 @@
-#include "Counter.hpp"
-
-void Counter::setValue(int value)
-{
-    if (value != m_value) {
-        m_value = value;
-        emit valueChanged(value);
-    }
-}
---
+++ server/src/main.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2020
+** B-CPP-500-REN-5-1-babel-kylian.balan
+** File description:
+** main
+*/
+
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "hello, world!" << std::endl;
+    return 0;
+}
