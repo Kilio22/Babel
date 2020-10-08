12e9f911683c19e629ba753ada81b7eb370b3538
agosselin <antoine.gosselin@epitech.eu>
2020-10-08 15:39:32 +0200

    add help flag

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -11,12 +11,20 @@
 #include "ClientException.hpp"
 #include <QtWidgets/QApplication>
 #include <iostream>
+#include <cstring>
 
 int main(int ac, char **av)
 {
     try {
         QApplication qApplication(ac, av);
 
+        if (ac == 2 && strcmp("-h", av[1]) == 0) {
+            std::cout << av[0] << ": serverIp port clientIp" << std::endl;
+            std::cout << "    serverIp : ip of the Babel server" << std::endl;
+            std::cout << "    port     : port of the Babel server" << std::endl;
+            std::cout << "    clientIp : ip of the client you are starting" << std::endl;
+            return 0;
+        }
         Babel::Client::ServiceLocator::getInstance().get<Babel::Client::BabelClient>().create(ac, av);
         Babel::Client::ServiceLocator::getInstance().get<Babel::Client::WindowManager>();
         qApplication.exec();
